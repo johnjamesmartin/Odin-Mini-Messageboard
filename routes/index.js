a3b88961ba86a14b84ce612dca728ae42b4f8236
John Martin <johnjamesmartin@live.com>
2019-07-03 00:48:24 +0100

    Add messages array

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hey everyone',
+    user: 'Alice'
+  },
+  {
+    text: 'Everyone doing well?',
+    user: 'Bob'
+  }
+];
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/new', (req, res) => {
+  res.render('new', { title: 'New' });
+});
+
+module.exports = router;
