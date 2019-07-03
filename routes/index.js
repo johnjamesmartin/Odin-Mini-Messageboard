581f977447b8094d8e994f3dba497df05bedac3c
John Martin <johnjamesmartin@live.com>
2019-07-03 01:31:20 +0100

    Implement form with ability to post new messages

--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,18 @@ router.get('/', (req, res, next) => {
   });
 });
 
+/* GET form. */
 router.get('/new', (req, res) => {
-  res.render('new', { title: 'New' });
+  res.render('form', { title: 'New Message' });
+});
+
+/* POST form. */
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
